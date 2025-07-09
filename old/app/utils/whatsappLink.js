491d96a50a7622493c3452d197e31984767aeab2
lazim-mv <lazimlatheef@gmail.com>
2025-07-09 14:44:49 +0530

    updated 2025 jul

--- old/app/utils/whatsappLink.js
+++
@@ -1,4 +0,0 @@
-export const getWhatsAppLink = (phoneNumber = "919947430188", message = "Hey Lazim! I found your website and would love to connect.") => {
-    const encodedMessage = encodeURIComponent(message);
-    return `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
-};
